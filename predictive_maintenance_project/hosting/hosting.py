2797138ba01a51f1a8a800e6b3d68be42b45ae2e
wash9968 <wash9968@gmail.com>
2025-12-15 00:01:37 +0530

    Update hosting.py

--- predictive_maintenance_project/hosting/hosting.py
+++ predictive_maintenance_project/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="predictive_maintenance_project/deployment",     # the local folder containing your files
-    repo_id="wash/Engine-Failure-Prediction",          # the target repo
+    repo_id="wash9968/Engine-Failure-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
