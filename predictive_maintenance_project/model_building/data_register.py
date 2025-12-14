b2b5c1386166062813244609631c2a5beafaa6b2
wash9968 <wash9968@gmail.com>
2025-12-14 14:10:21 +0000

    first commit

--- predictive_maintenance_project/model_building/data_register.py
+++ predictive_maintenance_project/model_building/data_register.py
@@ -19,7 +19,7 @@ except RepositoryNotFoundError:
     print(f"Space '{repo_id}' created.")
 
 api.upload_folder(
-    folder_path="predictive_maintainance_project/data",
+    folder_path="predictive_maintenance_project/data",
     repo_id=repo_id,
     repo_type=repo_type,
 )
